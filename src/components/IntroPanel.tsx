a65ad9012ca7612356c0e4bce7b9dd0b62414898
Kenjiro56 <hiroken506@gmail.com>
2024-11-20 12:21:03 +0900

    feat/row

--- src/components/IntroPanel.tsx
+++ src/components/IntroPanel.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Flex, Image, Heading, Text } from '@chakra-ui/react';
+import profile_img from '../assets/drawn.jpeg';
 
 
 const IntroPanel = () => {
   return (
-    <>
-      <div>
-          <img src={"../assets/drawn.jpeg"} alt={"Kenjiro Hirose"}/>
-      </div>
-    </>
+<Flex justify="center" align="center">
+
+        <div>
+          <Heading size="6xl">Kenjiro Hirose</Heading>
+          <Text>岐阜出身。名古屋工業大学大学を卒業後、大阪大学大学院へ進学。2025年卒業予定。</Text>
+        </div>
+        <Image
+            src={profile_img}
+            alt="Kenjiro Hirose"
+            borderRadius="full"
+            width="300px"
+          />
+        </Flex>
   );
 }
 
