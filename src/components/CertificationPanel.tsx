59f4e6fa9088dc976405c4eb64586600c3ea2da6
Kenjiro56 <hiroken506@gmail.com>
2024-11-21 11:30:15 +0900

    feat/certification

--- src/components/CertificationPanel.tsx
+++ src/components/CertificationPanel.tsx
@@ -1,17 +1,16 @@
+import {Box, Flex, Center, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const CertificationPanel = () => {
   return(
     <>
-              <h2 >Certification</h2>
-          <div>
-              <div>
-                  <p>学士（工学）</p>
-              </div>
-              <div>
-                  <p>基本情報技術者</p>
-              </div>
-          </div>
+      <Center h="100%">
+        <Heading size="3xl">Certification</Heading>
+      </Center>
+      <Flex justify="center" gap="4">
+        <Box borderWidth="2px" p="2">学士(工学)</Box>
+        <Box borderWidth="2px" p="2">基本情報技術者</Box>
+      </Flex>
     </>
   );
 }
--- src/components/IntroPanel.tsx
+++ src/components/IntroPanel.tsx
@@ -5,19 +5,18 @@ import profile_img from '../assets/drawn.jpeg';
 
 const IntroPanel = () => {
   return (
-<Flex justify="center" align="center">
-
-        <div>
-          <Heading size="6xl">Kenjiro Hirose</Heading>
-          <Text>岐阜出身。名古屋工業大学大学を卒業後、大阪大学大学院へ進学。2025年卒業予定。</Text>
-        </div>
-        <Image
-            src={profile_img}
-            alt="Kenjiro Hirose"
-            borderRadius="full"
-            width="300px"
-          />
-        </Flex>
+    <Flex justify="center" align="center">
+      <div>
+        <Heading size="6xl">Kenjiro Hirose</Heading>
+        <Text>岐阜出身。名古屋工業大学大学を卒業後、大阪大学大学院へ進学。2025年卒業予定。</Text>
+      </div>
+      <Image
+        src={profile_img}
+        alt="Kenjiro Hirose"
+        borderRadius="full"
+        width="300px"
+      />
+    </Flex>
   );
 }
 
