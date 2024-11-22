2d5be7cda5d03876e802a4fae1ecee506d5a6e0f
Kenjiro56 <hiroken506@gmail.com>
2024-11-22 12:07:21 +0900

    feat/links

--- src/components/LinksPanel.tsx
+++ src/components/LinksPanel.tsx
@@ -1,19 +1,35 @@
+import { Center, Heading, IconButton } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 import React from "react";
 const LinksPanel = () => {
   return (
     <>
-          {/* <div className="mt-4 flex gap-4">
-          <a href="https://zenn.dev/jiroken" target="_blank"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">Zenn</a>
-          <a href="https://qiita.com/Jiroken" target="_blank"
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">Qiita</a>
-          <a href="https://github.com/Kenjiro56" target="_blank"
-              className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full transition duration-300">GitHub</a>
-          <a href="https://twitter.com/r0se6K" target="_blank"
-              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">X</a>
-          <a href="https://jackmer.hatenablog.com/" target="_blank"
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition duration-300">Blog</a>
-      </div> */}
+      <Center h="100%">
+        <Heading size="3xl">
+          Links
+        </Heading>
+      </Center>
+      <IconButton>
+        <a href="https://zenn.dev/jiroken">
+        </a>
+      </IconButton>
+      <IconButton>
+        <a href="https://qiita.com/Jiroken">
+        </a>
+      </IconButton>
+      <IconButton>
+      <FaGithub />
+        <a href="https://github.com/Kenjiro56">
+        </a>
+      </IconButton>
+      <IconButton>
+        <a href="https://jackmer.hatenablog.com/">
+        </a>
+      </IconButton>
+      <IconButton>
+        <a href="https://twitter.com/r0se6K">
+        </a>
+      </IconButton>
     </>
   );
 };
