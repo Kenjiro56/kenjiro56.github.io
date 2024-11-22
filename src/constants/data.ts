b147f655c36d46630c1371ecb6ac14336278d5e7
Kenjiro56 <hiroken506@gmail.com>
2024-11-22 15:40:10 +0900

    add/images

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -5,6 +5,7 @@ export const CertificationText = [
 ]
 
 export const InterestText = [
+  'Cryptography',
   'HackTheBox',
   'TryHackMe',
   'Next.js',
@@ -23,7 +24,7 @@ export const ExperienceData = [
     src: 'https://life-is-tech.com/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://pbs.twimg.com/media/ECJzMyzUIAA7sVG?format=jpg&name=medium',
     category: 'Workshop',
     title: 'セキュリティキャンプ全国大会2019',
     date: '2019/8/13-2019/8/17',
@@ -31,7 +32,7 @@ export const ExperienceData = [
     src: 'https://life-is-tech.com/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://pbs.twimg.com/media/EI_fIkNUUAAhyuH?format=jpg&name=4096x4096',
     category: 'Workshop',
     title: 'セキュリティミニキャンプ in 北海道',
     date: '2019/11/9-2019/11/10',
@@ -39,7 +40,7 @@ export const ExperienceData = [
     src: 'https://life-is-tech.com/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://pbs.twimg.com/media/EGGNrgTU4AAmxGs?format=jpg&name=large',
     category: 'Workshop',
     title: 'セキュリティミニキャンプ in 愛知',
     date: '2019/10/5',
@@ -47,7 +48,7 @@ export const ExperienceData = [
     src: 'https://life-is-tech.com/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://pbs.twimg.com/media/GMO2ra3acAA9SN5?format=jpg&name=medium',
     category: 'Workshop',
     title: 'セキュリティミニキャンプ in 東京',
     date: '2024/4/27-2024/4/28',
@@ -63,15 +64,15 @@ export const ExperienceData = [
     src: 'https://nitsazanka.studio.site/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://user-images.githubusercontent.com/50067752/98429838-9a6ae700-20ec-11eb-88a2-85e01486bf66.png',
     category: 'Hackathon',
     title: 'JPHacks2020',
     date: '2020/10/26-2020/11/28',
     description: 'hogehoge',
-    src: 'https://jphacks.com/2020/',
+    src: 'https://github.com/jphacks/D_2013/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://nagoyaboost.jp/2021/assets/images/top/p_logo_nagoyaboost10000.png',
     category: 'Hackathon',
     title: 'NAGOYABOOST1000 2021',
     date: '2021/8-2022/2',
@@ -79,19 +80,19 @@ export const ExperienceData = [
     src: 'https://nagoyaboost.jp/2021/',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://ccc.code4japan.org/ccc-logo.svg',
     category: 'Hackathon',
     title: 'CivitechChallengeCup[U-22]',
     date: '2021/7-2021/9',
     description: 'hogehoge',
-    src: 'https://ccc2021.code4japan.org/',
+    src: 'https://github.com/hiromu617/loadmap-share',
   },
   {
-    imgsrc: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg',
+    imgsrc: 'https://s.yimg.jp/images/hacku/hacku2024/open/images/addition/open2024_passmarket_tokyo3.png',
     category: 'Hackathon',
     title: 'OpenHackU2024 Tokyo Vol.3',
     date: '2024/11/1-2024/11/16',
     description: 'hogehoge',
-    src: 'https://ccc2021.code4japan.org/',
+    src: 'https://github.com/Kenjiro56/HackU-Friday',
   },
 ]
