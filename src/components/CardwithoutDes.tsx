b61f8a798d3dd3cf990caee3e04b75b30fc004c4
Kenjiro56 <hiroken506@gmail.com>
2024-11-24 13:26:04 +0900

    fix/中央揃え

--- src/components/CardwithoutDes.tsx
+++ src/components/CardwithoutDes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Center, Flex, Heading } from "@chakra-ui/react";
+import { Box, Center, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 interface CardwithoutDesProps {
   title: string;
   contents: string[];
@@ -8,7 +8,9 @@ interface CardwithoutDesProps {
 const CardwithoutDes: React.FC<CardwithoutDesProps> = ( props ) => {
   const listItems = props.contents.map((content, index) => (
     <Box key={index} borderWidth="1px" p="2" borderColor='teal' rounded='md' shadow='md'>
-      {content}
+      <Center>
+        {content}
+      </Center>
     </Box>
   ));
   return (
@@ -16,9 +18,9 @@ const CardwithoutDes: React.FC<CardwithoutDesProps> = ( props ) => {
       <Center h="100%"  mt='4%' mb='1%'>
         <Heading size="3xl">{ props.title }</Heading>
       </Center>
-      <Flex justify="center" gap="4">
+      <SimpleGrid columns={[1, 4, 7]} gap="4">
         { listItems }
-      </Flex>
+      </SimpleGrid>
     </>
   );
 }
